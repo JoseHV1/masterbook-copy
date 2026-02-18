c6083b25b48bf264b4689d1b120ecda1700d8953
Jose Hernandez <jose@djobnet.com>
2026-02-18 16:22:49 -0400

    updated 18-02-2026

--- src/app/portal/accounts/accounts-details/accounts-details.component.ts
+++ src/app/portal/accounts/accounts-details/accounts-details.component.ts
@@ -28,6 +28,7 @@ import { InvoiceService } from '@app/shared/services/invoice.service';
 import { PaginatedResponse } from '@app/shared/interfaces/models/paginated-response.model';
 import { FilteredTable } from 'src/app/shared/classes/filtered-table-base/filtered-table.base';
 import { PageEvent } from '@angular/material/paginator';
+import { UploadFileService } from '@app/shared/services/upload_file.service';
 
 @Component({
   selector: 'app-accounts-details',
@@ -69,6 +70,8 @@ export class AccountsDetailsComponent {
     total_records: 0,
   };
   isOwner!: boolean;
+  accountProfileImgDefault = '/assets/icons/user_two.svg';
+  accountProfileImg: string = this.accountProfileImgDefault;
 
   constructor(
     private activateRoute: ActivatedRoute,
@@ -83,7 +86,8 @@ export class AccountsDetailsComponent {
     private _payments: PaymentsService,
     private _invoices: InvoiceService,
     private dialog: MatDialog,
-    private _auth: AuthService
+    private _auth: AuthService,
+    private _uploadFile: UploadFileService
   ) {
     this.isOwner = ownersRolesDataset.includes(
       this._auth.getAuth()?.user.role ?? RolesEnum.INSURED
@@ -149,6 +153,11 @@ export class AccountsDetailsComponent {
     return this._accounts.getAccountBySerial(id).pipe(
       tap(resp => {
         this.account = resp;
+        if (resp.user?.photo_url) {
+          this._uploadFile.getUrlFile(resp.user?.photo_url).subscribe(url => {
+            this.accountProfileImg = url ?? this.accountProfileImgDefault;
+          });
+        }
       })
     );
   }
--- src/app/portal/agency-settings/components/agency-settings-form/agency-settings-form.component.ts
+++ src/app/portal/agency-settings/components/agency-settings-form/agency-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { DatasetsService } from '@app/shared/services/dataset.service';
 import { UploadFileService } from '@app/shared/services/upload_file.service';
 import { finalize } from 'rxjs';
 import { UiModalTypeEnum } from 'src/app/shared/enums/ui-modal-type.enum';
@@ -7,6 +8,9 @@ import { AgencyModel } from 'src/app/shared/interfaces/models/agency.model';
 import { EditAgencySettingsRequest } from 'src/app/shared/interfaces/requests/agencies/edit-agency-settings.request';
 import { AgencySettingsService } from 'src/app/shared/services/agency-settings.service';
 import { UiService } from 'src/app/shared/services/ui.service';
+import { BusinessLineModel } from 'src/app/shared/interfaces/models/business-line.model';
+import { isInvalid } from 'src/app/shared/helpers/is-invalid.helper';
+import { hasError } from 'src/app/shared/helpers/has-error.helper.ts';
 
 @Component({
   selector: 'app-agency-settings-form',
@@ -17,13 +21,22 @@ export class AgencySettingsFormComponent implements OnInit {
   form!: FormGroup;
   dataAgency!: AgencyModel;
   logo?: string;
+  businessLines: BusinessLineModel[] = [];
+
+  isInvalid = isInvalid;
+  hasError = hasError;
 
   constructor(
     private _ui: UiService,
     private _agencySettings: AgencySettingsService,
-    private _uploadFile: UploadFileService
+    private _uploadFile: UploadFileService,
+    private _datasets: DatasetsService,
   ) {
     this.form = this._agencySettings.createEditSettingsForm();
+    this._datasets
+      .getBusinessLinesDataset()
+      .pipe(finalize(() => this._ui.hideLoader()))
+      .subscribe(businessLines => (this.businessLines = businessLines));
   }
 
   ngOnInit(): void {
@@ -42,9 +55,16 @@ export class AgencySettingsFormComponent implements OnInit {
   }
 
   fillData(agency: AgencyModel) {
+    const selectedLines = this.businessLines.filter(line =>
+      agency.business_lines.includes(line._id)
+    );
+
+    const selectedIds = selectedLines.map(line => line._id);
+
     this.form.patchValue({
       retentions: agency.retentions,
       taxes: agency.taxes,
+      business_lines_ids: selectedIds,
     });
   }
 
@@ -61,6 +81,10 @@ export class AgencySettingsFormComponent implements OnInit {
       retentions: this.form.value.retentions ?? null,
       taxes: this.form.value.taxes ?? null,
       logo_image: this.form.value.logo_image ?? undefined,
+      business_lines: this.form.value.business_lines_ids || [],
+      check_branding: !!(
+        (this.form.value.check_brand_publication as Array<string>) ?? []
+      ).length,
     };
 
     this._ui.showLoader();
